35f63cab1744e3f7835707f7d1c2d58df3fcbe5c
Moritz Plattner <moritz.plattner@gmx.net>
2020-01-27 13:16:14 +0100

    minor fixes + created a separate config file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## plain-html-bloger
+## plainhtml-blog-poster 
 This is a bash script to semi-automatically publish content to a plain-html blog-like website. It polls it's content from a local directory, downloads the current html file from the remote dir, adds the content and re-uploads it through scp.
 
 ### Localdir
@@ -22,9 +22,31 @@ The scripts expects a remote directory with the following contents:
 
 The folders are not automatically created - I was too lazy to automate a task that takes 30 seconds once a year. The "blog_$year.html" file can have any contents, as long as a specific marker (see included example file) is included. All submissions will be inserted after it.
 
-### Variables that can be changed in blog_post.sh
-- localdir - as long as the script lies in the same directory as "*.jpg", "blogpost.txt" and the "full"/"preview" folders, no changes are required.
-- sshcon - obviously, the place-holder username and domain have to be replaced by some that work on the webserver.
-- remotedir - path to the folder where the "blog_$year.html" is supposed to be - presumably somewhere in the webserver directory.
-- textpath - as long as "blogpost.txt" lies in localdir, no changes are required.
-- previewsize / fullsize - sizes for the pictures in px. Default values work fine for my application.
+### Variables that have to be conatined in "configPath" file
+- localDir - as long as the script lies in the same directory as "*.jpg", "blogpost.txt" and the "full"/"preview" folders, no changes are required.
+- sshCon - obviously, the place-holder username and domain have to be replaced by some that work on the webserver.
+- remoteDir - path to the folder where the "blog_$year.html" is supposed to be - presumably somewhere in the webserver directory.
+- textPath - as long as "blogpost.txt" lies in localdir, no changes are required.
+- previewSize / fullsize - sizes for the pictures in px. Default values work fine for my application.
+
+Example for "configPath" file:
+```bash
+localDir pwd
+sshCon user@domain
+remoteDir /path/to/folder
+textPath blogpost.txt
+previewSize "300"
+fullSize "1280x1280"
+```
+
+Example for "blogpost.txt" file:
+```bash
+# write the filename for the image into the next line:
+image_name.jpg
+
+# write the alternative text for the image into the next line:
+image_alt_text
+
+# write the text for the blog post into the next line:
+image_description
+```
--- blog_post.sh
+++ blog_post.sh
@@ -4,46 +4,55 @@
 # resize a picture at a specified path accordingly,
 # then insert it into my webserver structure (directly editing the html file)
 
-# setting a few paths and the ssh usernamename/domain
-localdir=`pwd`
-sshcon=username@yourdomain.tld
-remotedir=remote/webserver/filedir
-textpath=blogpost.txt
-previewsize="300"
-fullsize="1280x1280"
+# reading a few paths and the ssh usernamename/domain from $configPath file
+configPath=variables
+
+localDir=(`cat $configPath | awk -F"[ ]+" '/localDir /{printf $2 " "}'`)
+sshCon=(`cat $configPath | awk -F"[ ]+" '/sshCon /{printf $2 " "}'`)
+remoteDir=(`cat $configPath | awk -F"[ ]+" '/remoteDir /{printf $2 " "}'`)
+textPath=(`cat $configPath | awk -F"[ ]+" '/textPath /{printf $2 " "}'`)
+previewSize=(`cat $configPath | awk -F"[ ]+" '/previewSize /{printf $2 " "}'`)
+fullSize=(`cat $configPath | awk -F"[ ]+" '/fullSize /{printf $2 " "}'`)
+
+# If localDir has not been assigned in $configPath, use the current working directory instead as default
+if [ -z "$localDir" ]
+then
+      localDir=`pwd`
+fi
+
 
 # get the current date to be used in the post
-postdate=`date +%d.%m.%Y`
+postDate=`date +%d.%m.%Y`
 # the current year is used to enter the post into the right file
-postyear=`date +%Y`
+postYear=`date +%Y`
 
-# poll the $textpath file for the information needed to create the post
-imagename=`awk -F: '/write the filename for the image into the next line/ { getline; print $0 }' $localdir/$textpath`
-imagealt=`awk -F: '/write the alternative text for the image into the next line/ { getline; print $0 }' $localdir/$textpath`
-blogtext=`awk -F: '/write the text for the blog post into the next line/ { getline; print $0 }' $localdir/$textpath`
+# poll the $textPath file for the information needed to create the post
+imageName=`awk -F: '/write the filename for the image into the next line/ { getline; print $0 }' $localDir/$textPath`
+imageAlt=`awk -F: '/write the alternative text for the image into the next line/ { getline; print $0 }' $localDir/$textPath`
+blogText=`awk -F: '/write the text for the blog post into the next line/ { getline; print $0 }' $localDir/$textPath`
 
 # combine the polled values with some html syntax
-blogpost="<\!-- marker for auto-generated content insertion -->\n\n<div class=\"blog_post\">\n<p><a href=\"$postyear\/$imagename\"><img class=\"preview\" alt=\"$imagealt\" src=\"$postyear\/preview\/$imagename\">\n$postdate<\/a><br>\n$blogtext<\/div>"
+blogPost="<\!-- marker for auto-generated content insertion -->\n\n<div class=\"blog_post\">\n<p><a href=\"$postYear\/$imageName\"><img class=\"preview\" alt=\"$imageAlt\" src=\"$postYear\/preview\/$imageName\">\n$postDate<\/a><br>\n$blogText<\/div>"
 
 # copy the html file from the server to a local temp file
-scp $sshcon:$remotedir/blog_$postyear.html $localdir/blog_"$postyear"_temp.html && echo "html file has been copied to the local machine"
+scp $sshCon:$remoteDir/blog_$postYear.html $localDir/blog_"$postYear"_temp.html && echo "html file has been copied to the local machine"
 # edit the temp file by adding the new post in the old marker's place
-sed -i "s/<\!-- marker for auto-generated content insertion -->/$blogpost/g" $localdir/blog_"$postyear"_temp.html && echo "html file has been edited successfully"
+sed -i "s/<\!-- marker for auto-generated content insertion -->/$blogPost/g" $localDir/blog_"$postYear"_temp.html && echo "html file has been edited successfully"
 # copy the file back onto the server
-scp $localdir/blog_"$postyear"_temp.html $sshcon:$remotedir/blog_$postyear.html && echo "html file has been copied to the server"
+scp $localDir/blog_"$postYear"_temp.html $sshCon:$remoteDir/blog_$postYear.html && echo "html file has been copied to the server"
 
 # rename the local image to the desired name
-mv $localdir/*.jpg $localdir/$imagename
+mv $localDir/*.jpg $localDir/$imageName
 
 # resize the picture for both preview and full view resolution
-mogrify -path $localdir/full -resize $fullsize -quality 100 $localdir/*.jpg && echo "fullsize image has been created"
-mogrify -path $localdir/preview -resize $previewsize -quality 100 $localdir/*.jpg && echo "preview image has been created"
+mogrify -path $localDir/full -resize $fullSize -quality 100 $localDir/*.jpg && echo "fullSize image has been created"
+mogrify -path $localDir/preview -resize $previewSize -quality 100 $localDir/*.jpg && echo "preview image has been created"
 
 # copy the images from the local directories to the server over ssh
-scp $localdir/full/* $sshcon:$remotedir/$postyear && echo "fullsize image has been copied" && scp $localdir/preview/* $sshcon:$remotedir/$postyear/preview && echo "preview image has been copied"
+scp $localDir/full/* $sshCon:$remoteDir/$postYear && echo "fullSize image has been copied" && scp $localDir/preview/* $sshCon:$remoteDir/$postYear/preview && echo "preview image has been copied"
 
 # clean up local files
-rm $localdir/*.jpg
-rm $localdir/full/*
-rm $localdir/preview/*
-rm $localdir/blog_"$postyear"_temp.html
+rm $localDir/*.jpg
+rm $localDir/full/*
+rm $localDir/preview/*
+rm $localDir/blog_"$postYear"_temp.html
